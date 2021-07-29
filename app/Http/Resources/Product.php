6cd7dfaa0334a4eeedb3a1cc2b18fe4fdd1bdec7
StefanS97 <stefan.stojkov97@gmail.com>
2021-07-29 16:01:37 +0200

    Initial commit

---
+++ app/Http/Resources/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Product extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'detail' => $this->detail,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
